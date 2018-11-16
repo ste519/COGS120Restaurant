7c6000d88fb56101a4bb9c8eca0916d4c33b7b55
ste <ste@stedeMacBook-Pro.local>
2018-11-15 16:02:49 -0800

    Swap branches

--- js/lab4.js
+++ js/lab4.js
@@ -1,27 +1,15 @@
-  // This serves as a fake database:
-  // var BKtime = setInterval(){function(){Math.floor((Math.random()*60)+1);
-  // },500};
-  var time1 = Math.floor((Math.random()*60)+1);
-  var time2 = Math.floor((Math.random()*60)+1);
-  var time3 = Math.floor((Math.random()*60)+1);
-  var time4 = Math.floor((Math.random()*60)+1);
-  var time5 = Math.floor((Math.random()*60)+1);
-  var time6 = Math.floor((Math.random()*60)+1);
-  var time7 = Math.floor((Math.random()*60)+1);
-  var time8 = Math.floor((Math.random()*60)+1);
-  var time9 = Math.floor((Math.random()*60)+1);
-
+// This serves as a fake database:
 
  var complexData = [
-  {'title': 'Burger King', 'waitTime' : time1,'index': 1, 'ranking' : 0},
-  {'title': 'Tapioca', 'waitTime' : time2 , 'index': 2, 'ranking' : 0},
-  {'title': 'LemonGrass', 'waitTime' : time3, 'index': 3, 'ranking' : 0},
-  {'title': 'Subway', 'waitTime' : time4, 'index': 4, 'ranking' : 0},
-  {'title': 'Shogun', 'waitTime' : time5, 'index': 5, 'ranking' : 0},
-  {'title': 'Rubios', 'waitTime' : time6, 'index': 6, 'ranking' : 0},
-  {'title': 'Santorini', 'waitTime' : time7, 'index': 7, 'ranking' : 0},
-  {'title': 'Seed&Sprouts', 'waitTime' : time8, 'index': 8, 'ranking' : 0},
-  {'title': 'PandaExpress', 'waitTime' : time9, 'index': 9, 'ranking' : 0},
+  {'title': 'Burger King', 'waitTime' : 1 , 'index': 1, },
+  {'title': 'Tapioca', 'waitTime' : 3 , 'index': 2, },
+  {'title': 'LemonGrass', 'waitTime' : 4, 'index': 3, },
+  {'title': 'Subway', 'waitTime' : 6, 'index': 4, },
+  {'title': 'Shogun', 'waitTime' : 7, 'index': 5, },
+  {'title': 'Rubios', 'waitTime' : 8, 'index': 6, 'dsp' : 'none'},
+  {'title': 'Santorini', 'waitTime' : 11, 'index': 7, 'dsp' : 'none'},
+  {'title': 'Seed&Sprouts', 'waitTime' : 15, 'index': 8, 'dsp' : 'none'},
+  {'title': 'PandaExpress', 'waitTime' : 16, 'index': 9, 'dsp' : 'none'},
 ]
 
 // Call this function when the page loads (the "ready" event)
@@ -34,9 +22,7 @@ $(document).ready(function() {
   var parentDiv = $("#templatedProjects");
   var parentDiv2 = $("#FavoritesList");
 
-  complexData.sort(function(a, b) {
-  return parseFloat(a.waitTime) - parseFloat(b.waitTime);
-  });
+
   // now iterate through the complexData list and keep appending:
   for (var i = 0; i < complexData.length; i++) {
     var curData2 = JSON.parse(localStorage.getItem(complexData[i].title));
@@ -47,9 +33,9 @@ $(document).ready(function() {
   }
 
   for (var i = 0; i < complexData.length; i++) {
-      complexData[i].ranking = i+1;
-      var curData = complexData[i];
-      var curHtml = template(curData);
-      parentDiv.append(curHtml);
-    }
+    var curData = complexData[i];
+    var curHtml = template(curData);
+    parentDiv.append(curHtml);
+  }
+    console.log(curHtml);
 });
